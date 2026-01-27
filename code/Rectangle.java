32b0f3f775baaf10dc13fa48fd985d7e3a5f71f0
PriyanshuDave00 <pndave@ualberta.com>
2026-01-27 16:29:39 -0700

    Lab-4

---
+++ code/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle extends Shape {
+    int width;
+    int height;
+
+    // Constructor
+    public Rectangle(int x, int y, string colour) {
+        super(x, y, colour); // call the Shape constructor
+    }
+}
--- code/Shape.java
+++ code/Shape.java
@@ -1,10 +1,12 @@
 public abstract class Shape {
     int x;
     int y;
-
+    string colour;
+    string colour = "blue";
     // Constructor
-    public Shape(int x, int y) {
+    public Shape(int x, int y, string colour) {
         this.x = x;
         this.y = y;
+        this.colour = colour;
     }
 }
