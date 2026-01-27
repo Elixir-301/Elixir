9e910023dbd71bac3fe7db0cd710382cc5ac64b0
PriyanshuDave00 <pndave@ualberta.com>
2026-01-27 16:42:01 -0700

    Commit this change

--- code/Shape.java
+++ code/Shape.java
@@ -3,7 +3,6 @@ public abstract class Shape {
     int y;
     string colour =  "$PUT_YOUR_CHOICE_OF_COLOR";
 
->>>>>>> e8073a8e11b7d1420cb840199131336dd1a7c891
     // Constructor
     public Shape(int x, int y, string colour) {
         this.x = x;
