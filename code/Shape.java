e8073a8e11b7d1420cb840199131336dd1a7c891
PriyanshuDave00 <pndave@ualberta.ca>
2026-01-27 16:39:06 -0700

    Add colour parameter to Shape constructor
    Updated Shape class to include colour parameter in constructor.

--- code/Shape.java
+++ code/Shape.java
@@ -1,10 +1,12 @@
 public abstract class Shape {
     int x;
     int y;
+    string colour =  "$PUT_YOUR_CHOICE_OF_COLOR";
 
     // Constructor
-    public Shape(int x, int y) {
+    public Shape(int x, int y, string colour) {
         this.x = x;
         this.y = y;
+        this.colour = colour
     }
 }
