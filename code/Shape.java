cd72a849ed168081ddf4470740e3b1fdcb7451d0
ahyan006 <ahyan_amin@outlook.com>
2026-01-30 15:54:08 -0700

    Add diamond shape

--- code/Shape.java
+++ code/Shape.java
@@ -1,12 +1,12 @@
 public abstract class Shape {
     int x;
     int y;
-    String color = "yellow";
+    string colour =  "$Black";
 
     // Constructor
     public Shape(int x, int y, string colour) {
         this.x = x;
         this.y = y;
-        this.colour = colour;
+        this.colour = colour
     }
 }
