791ac3f846cdb393d9b1f3497275e8235be3c5bb
Faiz Malik <82890623+faiz1357@users.noreply.github.com>
2026-01-30 13:56:14 -0700

    Update default color in Shape class
    Changed default color from placeholder to 'Purple'.

--- code/Shape.java
+++ code/Shape.java
@@ -1,7 +1,7 @@
 public abstract class Shape {
     int x;
     int y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "Purple";
 
     // Constructor
     public Shape(int x, int y, string colour) {
