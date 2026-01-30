e12891a423cb283980da755f0994585bb26f78c8
jordanantonio <jordanantonio727@gmail.com>
2026-01-30 11:55:12 -0700

    Add Circle model

---
+++ code/Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle extends Shape {
+    int base;
+    int height;
+
+    public Triangle(int x, int y, string colour, int base, int height) {
+        super(x, y, colour);
+        this.base = base;
+        this.height = height;
+    }
+}
